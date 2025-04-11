d5988852add73d754c5b40dfc81277858fef35ac
ayushghimiresharma95 <157111797+ayushghimiresharma95@users.noreply.github.com>
2025-04-11 20:02:05 +0300

    Teacher enity is added

--- setup.sh
+++ setup.sh
@@ -235,16 +235,21 @@ echo
 echo "Installation completed!"
 echo
 
+
+
+# TESTS
+if [ "$RUN_TESTS" = "true" ]; then
+  /bin/bash "$SCRIPT_DIR/scripts/run_tests.sh"
+fi
+
+cho "Running Kubernetes SSL configuration..."
 if [[ "$(uname)" == "Darwin" ]]; then
   bash "$SCRIPT_DIR/scripts/Kubernetes_ssl_configmap_creation.sh"  # Using default bash because /bin/bash is an old version (3)
 else
   /bin/bash "$SCRIPT_DIR/scripts/Kubernetes_ssl_configmap_creation.sh"
 fi
 
-# TESTS
-if [ "$RUN_TESTS" = "true" ]; then
-  /bin/bash "$SCRIPT_DIR/scripts/run_tests.sh"
-fi
+echo "✅ Kubernetes SSL configuration completed!"
 
 
 
