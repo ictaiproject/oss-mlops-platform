e9e4385152ef3546eb2052dad0a13984018bd3a9
ayushghimiresharma95 <157111797+ayushghimiresharma95@users.noreply.github.com>
2025-04-25 11:24:55 +0300

    Cert-manager kustomization file is updated

--- uninstall.sh
+++ uninstall.sh
@@ -137,7 +137,7 @@ if [ "$INSTALL_LOCAL_REGISTRY" = "true" ]; then
 fi
 
 # Define config files to delete
-ENV_FILE="$SCRIPT_DIR/deployment/kubeflow/manifests/common/cert-manager/cert-manager/base/config.env"
+ENV_FILE="$SCRIPT_DIR/deployment/kubeflow/manifests/common/cert-manager/cert-manager/overlay/$SSL_PROVIDER/config.env"
 MLFLOW_FILE="$SCRIPT_DIR/deployment/mlflow/base/config.env"
 KUBEFLOW_FILE="$SCRIPT_DIR/deployment/kubeflow/manifests/apps/pipeline/upstream/base/pipeline/config.env"
 GRAFANA_FILE="$SCRIPT_DIR/deployment/monitoring/grafana/config.env"
